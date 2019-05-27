f5d56e06d15fb2000322edbc35c0f84c81e6f5a5
Gingo Esau <jadegeau@gmail.com>
2019-05-27 19:38:52 +0300

    First commit

---
+++ src/pages/nextpage/nextpage.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { NextpagePage } from './nextpage';
+
+@NgModule({
+  declarations: [
+    NextpagePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(NextpagePage),
+  ],
+})
+export class NextpagePageModule {}
